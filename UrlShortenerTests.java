a9a41388a363a26d72035e26f7918ff73309f328
Sud555 <73770652+Sud555@users.noreply.github.com>
2025-07-05 14:09:37 +0530

    Add files via upload

---
+++ UrlShortenerTests.java
@@ -0,0 +1,11 @@
+package com.example.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UrlShortenerTests {
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
